538db002b6a9a3b73964ce16a2e786eb737e0937
Jonas Ådahl <jadahl@gmail.com>
2010-01-17 14:32:13 +0100

    Added a news post and google analytics javascript blob.

--- src/cms/cms_db.erl
+++ src/cms/cms_db.erl
@@ -11,6 +11,14 @@ post(_Post) ->
 
 get_all() ->
   [#content{
+      id = 124153,
+      timestamp = 1236930795, %% 2009-11-28 13:19
+      authors = ["Jonas"],
+      content = #blog_post{
+	title = "Jabber.CD.Chalmers.Se",
+	body = "At one point, Chalmers will discontinue to administer the DNS alias that is jabber.cd.chalmers.se. This means that at some point, accounts under the jabber.cd.chalmers domain will stop working as before. The accounts will still be available for use, but you would have to manually set your XMPP client to connect to \"jabber.se\". Affected accounts will also not be able to communicate with any accounts except the ones on jabber.cd.chalmers.se and jabber.se. We strongly suggest you to create a new account on jabber.se.",
+	tags = ["news"]}},
+  #content{
       id = 124152,
       timestamp = 1236930795,
       authors = ["Jonas"],
