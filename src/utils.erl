8979de59307ad07e93e43a90ffdb1aa0d3fd73b6
Jonas Ådahl <jadahl@gmail.com>
2011-01-12 22:43:30 +0100

    Some cleanup of utils.erl.

--- src/utils.erl
+++ src/utils.erl
@@ -24,8 +24,10 @@
         ts_to_date_s/1, ts_to_date/1, ts_to_hour_min/1, time_to_iso8601/1,
         to_binary/1, to_string/1, to_atom/1,
         sub_id/2,
-        to_xml/1, t_to_ht/1, ts_to_ht/1, text_to_hyper_text/1, texts_to_hyper_text/1, text_to_ht/1, log/5,
-        join/2, range/1, range/2, just/1, find_with/3, forall/2, keyreplacewith/4, keyreplaceoraddwith/4
+        to_xml/1,
+        log/5,
+        join/2, range/1, range/2, just/1, find_with/3, forall/2,
+        keyreplacewith/4, keyreplaceoraddwith/4
     ]).
 
 %
@@ -107,41 +109,6 @@ to_xml({Name, Attrs, Content}) ->
     wf_tags:emit_tag(Name, to_xml(Content), Attrs);
 to_xml({Name, Content}) ->
     wf_tags:emit_tag(Name, to_xml(Content), []).
-
-%
-% Hyper text
-%
-
-t_to_ht(T) -> text_to_hyper_text(T).
-ts_to_ht(Ts) -> texts_to_hyper_text(Ts).
-
-text_to_hyper_text([E | Es]) when is_integer(E) ->
-    #p{body = [E | Es]};
-text_to_hyper_text(Es) when is_list(Es) ->
-    texts_to_hyper_text(Es);
-text_to_hyper_text(E) ->
-    text_to_ht(E).
-
-texts_to_hyper_text(Texts) when is_list(Texts) ->
-    lists:map(fun text_to_ht/1, Texts);
-texts_to_hyper_text(_) ->
-    [].
-
-text_to_ht(T) when is_list(T) ->
-    T;
-text_to_ht({email, EMail}) ->
-    #link{url = "mailto:" ++ EMail, text = EMail};
-text_to_ht({url, URL}) ->
-    #link{url = URL, text = URL};
-text_to_ht({link, URL, Text}) ->
-    #link{url = URL, text = Text};
-text_to_ht({jid, JID}) ->
-    #link{url = "xmpp:" ++ JID, text = JID};
-text_to_ht({muc, MUC}) ->
-    #link{url = "xmpp:" ++ MUC ++ "?join", text = MUC};
-text_to_ht(T) ->
-    ?LOG_WARNING("Format ~p not recognized.", [T]),
-    [].
 
 %
 % Logging
